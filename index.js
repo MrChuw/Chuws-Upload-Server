43d590103b81d3d0f860d8beb4676d15b19548cd
MrChuw <jgabrielmonteiro@hotmail.com>
2022-12-03 13:41:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const saveServer = require("./server/index");
 // If run directly (i.e. through "npm start")
 if (require.main === module) {
-	saveServer(process.env.port || 80);
+	saveServer(process.env.port || 7776);
 }
 module.exports = function (port) {
 	const portToUse = port || process.env.port || 80;
